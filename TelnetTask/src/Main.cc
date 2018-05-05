c866ebeb08fb6f0c2c68100dc45b556b44ef2105
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2018-05-05 18:11:50 +0200

    Port number is now passed as first program's argument

--- TelnetTask/src/Main.cc
+++ TelnetTask/src/Main.cc
@@ -4,17 +4,33 @@
 #include "Telnet.hpp"
 #include "RemoteTerminal.hpp"
 #include "MenuApplication.hpp"
+#include "Util.hpp"
 
 
-int main()
+int main(int argc, char **argv)
 {
-    srand(static_cast<unsigned int>(time(nullptr)));
-    TCPSocket socket(static_cast<uint16_t>(10000 + (rand() % 100)));
-    std::clog << "MAIN  : Listening on port " << socket.port() << std::endl;
+    if (argc < 2) {
+        std::cerr << "USAGE: " << argv[0] << " {PORT_NUMBER}" << std::endl;
+        return 1;
+    }
+    long unsigned int portnum = strtoul(argv[1], nullptr, 10);
+    if (portnum < 1 || portnum > 65535) {
+        std::cerr << "Invalid port number" << std::endl;
+        return 1;
+    }
+
+    TCPSocket *socket = nullptr;
+    try {
+        socket = new TCPSocket(static_cast<uint16_t>(portnum));
+    } catch (SystemError &e) {
+        std::cerr << "Failed to bind the port: " << e.what() << std::endl;
+        return 1;
+    }
+    std::clog << "MAIN  : Listening on port " << socket->port() << std::endl;
 
     while (true) {
         try {
-            MenuApplication menus(socket.accept());
+            MenuApplication menus(socket->accept());
             menus.run();
         } catch (std::runtime_error &e) {
             std::clog << "MAIN: error occured: " << e.what() << std::endl;
